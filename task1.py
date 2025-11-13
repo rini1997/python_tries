8b86fe354641a50e72aa983cd602d94b9e50ae4b
Rini020 <157001055+Rini020@users.noreply.github.com>
2025-11-13 11:45:30 +0530

    first commit

---
+++ task1.py
@@ -0,0 +1,16 @@
+"""Task 1: Perform Basic Mathematical Operations
+Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen"""
+num1= input("please Enter the first number : ")
+num2= input("please Enter the second number : ")
+print("Addition : ", int(num1)+int(num2))
+print("Substraction : ", int(num1)-int(num2))
+print("Multiplication : ", int(num1)*int(num2))
+print("Division : ", int(num1)/int(num2))
+
